2e7e3edc6a181672ee8d9140582cb30d9e34f387
aivaras.armalis <176276546+Aivaras-Net@users.noreply.github.com>
2025-04-23 17:06:15 +0300

    feat: add rank and latest comment rankings in home page

--- src/Blog/Controllers/HomeController.cs
+++ src/Blog/Controllers/HomeController.cs
@@ -1,21 +1,57 @@
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Blog.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IArticleService _articleService;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IArticleService articleService,
+            ApplicationDbContext context)
         {
             _logger = logger;
+            _articleService = articleService;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                RecentArticles = await _context.Articles
+                    .Include(a => a.Author)
+                    .Include(a => a.Votes)
+                    .OrderByDescending(a => a.PublishedDate)
+                    .Take(5)
+                    .ToListAsync(),
+
+                TopRankedArticles = await _context.Articles
+                    .Include(a => a.Author)
+                    .Include(a => a.Votes)
+                    .OrderByDescending(a => a.Votes.Sum(v => v.IsUpvote ? 1 : -1))
+                    .ThenByDescending(a => a.PublishedDate)
+                    .Take(3)
+                    .ToListAsync(),
+
+                RecentlyCommentedArticles = await _context.Articles
+                    .Include(a => a.Author)
+                    .Include(a => a.Votes)
+                    .Include(a => a.Comments)
+                    .Where(a => a.Comments.Any())
+                    .OrderByDescending(a => a.Comments.Max(c => c.CreatedAt))
+                    .Take(3)
+                    .ToListAsync()
+            };
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
